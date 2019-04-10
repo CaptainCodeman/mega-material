fec720f37c9363895e112aabd8bd666099a4826e
Simon Green <simon@captaincodeman.com>
2019-04-10 11:58:50 -0600

    Prep for publishing / linking

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,6 @@ import { terser } from 'rollup-plugin-terser';
 // assume production mode for normal build, dev if watched
 // flag is used to enable / disable HTML & JS minification
 const production = !process.env.ROLLUP_WATCH;
-const outputDir = process.env.OUTPUT_DIR || 'docs';
 const plugins = [
   resolve(),
   production && minifyHTML(),
@@ -54,7 +53,7 @@ export default [{
     'src/scratch.ts',
   ],
   output: {
-    dir: outputDir,
+    dir: 'docs',
     format: 'esm',
     sourcemap: false,
     entryFileNames: '[name].js',
@@ -62,19 +61,45 @@ export default [{
   },
   plugins,
 }, {
-  input: 'src/index.ts',
+  input: [
+    'src/button.ts',
+    'src/card.ts',
+    'src/checkbox.ts',
+    'src/chips.ts',
+    'src/drawer.ts',
+    'src/fab.ts',
+    'src/icon.ts',
+    'src/icon-button.ts',
+    'src/linear-progress.ts',
+    'src/list.ts',
+    'src/notched-outline.ts',
+    'src/ripple.ts',
+    'src/slider.ts',
+    'src/switch.ts',
+    'src/snackbar.ts',
+    'src/tabs.ts',
+    'src/textfield.ts',
+    'src/top-app-bar.ts',
+  ],
   output: {
-    file: outputDir + '/' + pkg.browser,
-    format: 'iife',
-    name: 'MegaMaterial',
+    dir: 'dist',
+    format: 'esm',
     sourcemap: false,
+    entryFileNames: '[name].js',
+    chunkFileNames: 'common.js',
   },
+  external: [
+    'tslib',
+    'lit-html',
+    'lit-element',
+  ],
   plugins,
 }, {
   input: 'src/index.ts',
   output: {
-    file: outputDir + '/' + pkg.main,
-    format: 'cjs',
+    file: 'dist/' + pkg.browser,
+    format: 'iife',
+    name: 'MegaMaterial',
     sourcemap: false,
   },
   plugins,
