a56a5b99eded58a6bd94b10cad3a6a329c9b1d91
Simon Green <simon@captaincodeman.com>
2019-04-14 10:17:53 -0600

    Initial tabs implementation

--- src/tabs.ts
+++ src/tabs.ts
@@ -3,6 +3,7 @@ import { LitElement, html, customElement, css, property, query } from 'lit-eleme
 import './icon'
 import './ripple'
 import { defaultCSS } from './styles';
+import { nothing } from 'lit-html';
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -13,7 +14,7 @@ declare global {
   }
 }
 
-export type Align = 'start' | 'end' | 'center'
+export type TabAlign = 'start' | 'end' | 'center'
 
 
 @customElement('mwc-tab')
@@ -23,6 +24,9 @@ export class TabElement extends LitElement {
 
   @property({ type: String })
   label = '';
+
+  @property({ type: Boolean, reflect: true })
+  active = false;
 
   @property({ type: Boolean, reflect: true })
   stacked = false;
@@ -40,6 +44,18 @@ export class TabElement extends LitElement {
     return [
       defaultCSS,
       css`
+:host {
+  flex: 1 0 auto;
+  justify-content: center;
+  white-space: nowrap;
+  position: relative;
+  contain: content;
+}
+mwc-ripple {
+  display: flex;
+  flex: 1 0 auto;
+  justify-content: center;
+}
 button {
   position: relative;
   font-family: Roboto, sans-serif;
@@ -52,8 +68,6 @@ button {
   text-decoration: none;
   text-transform: uppercase;
   display: flex;
-  flex: 1 0 auto;
-  justify-content: center;
   box-sizing: border-box;
   height: 48px;
   padding: 0 24px;
@@ -66,35 +80,24 @@ button {
   -webkit-appearance: none;
   z-index: 1;
 }
-
-:host {
-  outline: none;
-  flex: 1 0 auto;
-  display: flex;
-  justify-content: center;
+#label {
+  color: var(--mdc-theme-on-surface, #000);
 }
-
-:host {
-  margin-left: 0;
-  margin-right: 0;
+mwc-icon,
+slot[name="icon"]::slotted(*) {
+  color: var(--mdc-theme-on-surface, #000);
+  fill: currentColor;
 }
-
-:host::-moz-focus-inner {
+button::-moz-focus-inner {
+  padding: 0;
   border: 0;
 }
 
-.text-label {
-  color: var(--mdc-theme-on-surface, #000);
-}
-mwc-icon {
-  color: var(--mdc-theme-on-surface, #000);
-}
-
-:host([min-width]) {
+:host([min-width]) button {
   flex: 0 1 auto;
 }
 
-:host([content]) {
+#content {
   position: relative;
   display: flex;
   align-items: center;
@@ -103,79 +106,90 @@ mwc-icon {
   pointer-events: none;
 }
 
-.text-label,
-mwc-icon {
-  transition: 150ms color linear,
-              150ms opacity linear;
+#label,
+mwc-icon,
+slot[name="icon"]::slotted(*) {
+  transition: 150ms color linear, 150ms opacity linear;
   z-index: 2;
 }
 
-.text-label {
+#label {
   display: inline-block;
   opacity: 0.6;
   line-height: 1;
 }
 
-mwc-icon {
+mwc-icon,
+slot[name="icon"]::slotted(*) {
   width: 24px;
   height: 24px;
   opacity: 0.54;
   font-size: 24px;
+  --mdc-icon-size: 24px;
 }
 
-:host([stacked]) {
+:host([stacked]) button {
   height: 72px;
 }
 
-:host([stacked]) .content {
+:host([stacked]) #content {
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
 }
 
-:host([stacked]) mwc-icon {
+:host([stacked]) mwc-icon,
+:host([stacked]) slot[name="icon"]::slotted(*) {
   padding-top: 12px;
 }
 
-:host([stacked]) .text-label {
+:host([stacked]) #label {
   padding-bottom: 16px;
 }
 
-:host([active]) .text-label {
+:host([active]) #label {
   color: var(--mdc-theme-primary, #6200ee);
 }
-:host([active]) mwc-icon {
+:host([active]) mwc-icon,
+:host([active]) slot[name="icon"]::slotted(*) {
   color: var(--mdc-theme-primary, #6200ee);
+  fill: currentColor;
 }
-:host([active]) .text-label,
-:host([active]) mwc-icon {
+:host([active]) #label,
+:host([active]) mwc-icon,
+:host([active]) slot[name="icon"]::slotted(*) {
   transition-delay: 100ms;
   opacity: 1;
 }
 
-:host(:not([stacked])) mwc-icon + .text-label {
+:host(:not([stacked])) mwc-icon + #label,
+:host(:not([stacked])) slot[name="icon"] + #label {
   padding-left: 8px;
   padding-right: 0;
-}`
+}
+`
     ]
   }
 
   render() {
     return html`
-<button role="tab" ?aria-selected=${this.selected}>
-  <span class="content">
-    <mwc-icon>${this.icon}</mwc-icon>
-    <span class="text-label"><slot></slot></span>
-  </span>
-  <mwc-tab-indicator></mwc-tab-indicator>
-</button>`
+<mwc-ripple primary>
+  <button role="tab" ?aria-selected=${this.selected} tabindex="0">
+    <span id="content">
+      <slot name="icon">${this.icon ? html`<mwc-icon>${this.icon}</mwc-icon>` : nothing }</slot>
+      <span id="label"><slot></slot></span>
+    </span>
+    <mwc-tab-indicator .active=${this.active}></mwc-tab-indicator>
+  </button>
+</mwc-ripple>
+`
   }
 }
 
 @customElement('mwc-tab-bar')
 export class TabBarElement extends LitElement {
   @property({ type: String, reflect: true })
-  align: Align = 'start'
+  align: TabAlign = 'center'
 
   static get styles() {
     return [
@@ -183,25 +197,26 @@ export class TabBarElement extends LitElement {
       css`
 :host {
   width: 100%;
-}`
+}
+`
     ]
   }
 
   render() {
-    return html`
-<mwc-tab-scroller .align=${this.align}>
-  <slot></slot>
-</mwc-tab-scroller>`
+    return html`<mwc-tab-scroller .align=${this.align}><slot></slot></mwc-tab-scroller>`
   }
 }
 
 @customElement('mwc-tab-indicator')
 export class TabIndicatorElement extends LitElement {
+  @property({ type: Boolean, reflect: true })
+  active = false;
+
   static get styles() {
     return [
       defaultCSS,
       css`
-.mdc-tab-indicator {
+:host {
   display: flex;
   position: absolute;
   top: 0;
@@ -211,10 +226,8 @@ export class TabIndicatorElement extends LitElement {
   pointer-events: none;
   z-index: 1;
 }
-.mdc-tab-indicator > .mdc-tab-indicator__content--underline {
+#underline {
   background-color: var(--mdc-theme-primary, #6200ee);
-}
-.mdc-tab-indicator > .mdc-tab-indicator__content--underline {
   height: 2px;
 }
 .mdc-tab-indicator > .mdc-tab-indicator__content--icon {
@@ -258,22 +271,20 @@ export class TabIndicatorElement extends LitElement {
 
 .mdc-tab-indicator--active.mdc-tab-indicator--fade > .mdc-tab-indicator__content {
   transition-delay: 100ms;
-}`
+}
+`
     ]
   }
 
   render() {
-    return html`
-<span class="indicator indicator--active">
-  <span class="content content--underline"></span>
-</span>`
+    return html``
   }
 }
 
 @customElement('mwc-tab-scroller')
 export class TabScrollerElement extends LitElement {
   @property({ type: String, reflect: true })
-  align: Align = 'start'
+  align: TabAlign = 'center'
 
   @property({ type: Boolean, reflect: true })
   animating = false
@@ -289,18 +300,18 @@ export class TabScrollerElement extends LitElement {
   flex: 1;
 }
 
-.area {
+#area {
   width: 100%;
   -webkit-overflow-scrolling: touch;
   display: flex;
   overflow-x: scroll;
 }
 
-.area::-webkit-scrollbar {
+#area::-webkit-scrollbar {
   display: none;
 }
 
-.content {
+#content {
   position: relative;
   display: flex;
   flex: 1 0 auto;
@@ -308,35 +319,29 @@ export class TabScrollerElement extends LitElement {
   will-change: transform;
 }
 
-:host([align="start"]) .content {
+:host([align="start"]) #content {
   justify-content: flex-start;
 }
 
-:host([align="end"]) .content {
+:host([align="end"]) #content {
   justify-content: flex-end;
 }
 
-:host([align="center"]) .content {
+:host([align="center"]) #content {
   justify-content: center;
 }
 
-:host([animating]) .area {
+:host([animating]) #area {
   -webkit-overflow-scrolling: auto;
 }
 
-:host([animating]) .content {
+:host([animating]) #content {
   transition: 250ms transform cubic-bezier(0.4, 0, 0.2, 1);
 }`
     ]
   }
 
   render() {
-    return html`
-<div class="area">
-  <div class="content">
-    <slot></slot>
-  </div>
-</div>
-`
+    return html`<div id="area"><div id="content"><slot></slot></div></div>`
   }
 }
