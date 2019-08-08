86670ced1aa246a340270ada547c8e9fcb7148b8
Simon Green <simon@captaincodeman.com>
2019-08-08 10:25:03 -0600

    Basic storybook setup

---
+++ stories/linear-progress.stories.js
@@ -0,0 +1,22 @@
+import { storiesOf } from '@storybook/polymer';
+import { withKnobs, number, boolean } from '@storybook/addon-knobs/polymer';
+
+import '../dist/linear-progress.js'
+
+const percentageRange = {
+  range: true,
+  min: 0,
+  max: 1,
+  step: 0.01,
+};
+
+storiesOf('linear-progress', module)
+  .addDecorator(withKnobs)
+  .add('default', () => {
+    const el = document.createElement('mega-linear-progress');
+    el.progress = number('Progress', 0.5, percentageRange);
+    el.buffer = number('Buffer', 0.8, percentageRange);
+    el.reverse = boolean('Reversed', false);
+    el.indeterminate = boolean('indeterminate', true);
+    return el;
+  })
